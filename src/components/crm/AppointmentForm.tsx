4d1e7c05095230301e59bd02578877ddee5a6ced
[dyad] <daniele.buatti@gmail.com>
2026-01-28 17:46:15 +1100

    [dyad] Allowing past dates in the appointment form - wrote 1 file(s)

--- src/components/crm/AppointmentForm.tsx
+++ src/components/crm/AppointmentForm.tsx
@@ -189,9 +189,6 @@ const AppointmentForm = ({ onSuccess, initialClientId }: AppointmentFormProps) =
                       mode="single"
                       selected={field.value}
                       onSelect={field.onChange}
-                      disabled={(date) =>
-                        date < new Date(new Date().setHours(0, 0, 0, 0))
-                      }
                       initialFocus
                     />
                   </PopoverContent>

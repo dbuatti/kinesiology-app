0471d2ed13dd5002d75f9b1f85449847b360d1b4
[dyad] <daniele.buatti@gmail.com>
2026-02-13 16:53:43 +1100

    [dyad] Fixing TypeScript error in AppointmentsPage by making the grouping utility generic. - wrote 1 file(s)

--- src/utils/crm-utils.ts
+++ src/utils/crm-utils.ts
@@ -33,8 +33,8 @@ export const getClientRollups = (appointments: Appointment[]) => {
   };
 };
 
-export const groupAppointmentsByMonth = (appointments: Appointment[]) => {
-  const groups: Record<string, Appointment[]> = {};
+export const groupAppointmentsByMonth = <T extends Appointment>(appointments: T[]) => {
+  const groups: Record<string, T[]> = {};
   
   appointments.forEach(app => {
     const key = format(app.date, "MMMM yyyy");
@@ -44,5 +44,5 @@ export const groupAppointmentsByMonth = (appointments: Appointment[]) => {
   
   return Object.entries(groups).sort((a, b) => {
     return new Date(b[1][0].date).getTime() - new Date(a[1][0].date).getTime();
-  });
+  }) as [string, T[]][];
 };

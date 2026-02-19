c383fb1a9bbbb6651611b7763d2eca4b425a66f1
[dyad] <daniele.buatti@gmail.com>
2026-02-20 10:58:42 +1100

    [dyad] Fixing missing Button import in Sidebar - wrote 1 file(s)

--- src/components/crm/Sidebar.tsx
+++ src/components/crm/Sidebar.tsx
@@ -32,6 +32,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 import ClientForm from "./ClientForm";
 import AppointmentForm from "./AppointmentForm";
 

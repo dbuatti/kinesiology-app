7d22fe68f8355ea348bd962b9af85e2513c83b25
[dyad] <daniele.buatti@gmail.com>
2026-03-12 13:55:06 +1100

    Reverted all changes back to version b1912c0a63149dfc35c87bf56bbd9e18c7d3e978

--- src/components/crm/SessionContentSwitcher.tsx
+++ src/components/crm/SessionContentSwitcher.tsx
@@ -27,7 +27,7 @@ import EmotionAssessment from './EmotionAssessment';
 import PreviousSessionSummary from './PreviousSessionSummary';
 import GaitReflexAssessment from './GaitReflexAssessment';
 import PathwayAssessment from './PathwayAssessment';
-import CalibrationTab from './session-tabs/CalibrationTab';
+import PathwayLogicWizard from './PathwayLogicWizard';
 
 type ActiveView = 'home' | 'kinesiology' | 'muscles' | 'gait' | 'previous';
 
@@ -107,7 +107,10 @@ const SessionContentSwitcher = ({ appointment, onUpdate, saveField }: SessionCon
           </TabsContent>
 
           <TabsContent value="calibration" className="focus-visible:ring-0">
-            <CalibrationTab appointment={appointment} saveField={saveField} />
+            <PathwayLogicWizard
+              onSave={(summary) => saveField('modes_balances', summary)}
+              priorityPattern={appointment.priority_pattern}
+            />
           </TabsContent>
 
           <TabsContent value="reassessment" className="focus-visible:ring-0">

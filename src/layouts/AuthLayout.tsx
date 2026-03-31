1b32358531989c30b89489eeeecdb7806fc86676
[dyad] <daniele.buatti@gmail.com>
2026-04-01 10:23:17 +1100

    [dyad] Fixing onboarding page width constraint - wrote 1 file(s)

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -5,8 +5,8 @@ import { Outlet } from 'react-router-dom';
 
 const AuthLayout = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-slate-50 p-4">
-      <div className="w-full max-w-md">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-slate-50">
+      <div className="w-full">
         <Outlet />
       </div>
     </div>

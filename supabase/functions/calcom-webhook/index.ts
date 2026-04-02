6539468893ffa12e72edb0f2f293263c76d796a4
[dyad] <daniele.buatti@gmail.com>
2026-04-02 18:14:17 +1100

    [dyad] Added daniele.buatti@gmail.com as a BCC recipient to all onboarding emails. - wrote 2 file(s)

--- supabase/functions/calcom-webhook/index.ts
+++ supabase/functions/calcom-webhook/index.ts
@@ -30,6 +30,7 @@ async function sendGmail(accessToken: string, from: string, to: string, subject:
   const messageParts = [
     `From: ${from}`,
     `To: ${to}`,
+    `Bcc: daniele.buatti@gmail.com`,
     `Content-Type: text/html; charset=utf-8`,
     `MIME-Version: 1.0`,
     `Subject: ${utf8Subject}`,

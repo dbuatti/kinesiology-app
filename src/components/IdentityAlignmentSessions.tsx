4784dba3f4880d3b1e4fd9c29a5af4d68f9d273c
[dyad] <daniele.buatti@gmail.com>
2026-04-28 17:23:35 +1000

    [dyad] Fixing TypeScript errors in IdentityAlignmentSessions - wrote 1 file(s)

--- src/components/IdentityAlignmentSessions.tsx
+++ src/components/IdentityAlignmentSessions.tsx
@@ -1,2 +1,7 @@
-const response = await fetch(xebtjnvfkroiplyzftas.supabase.co/rest/v1/identity_alignment_sessions?select=);
+import { supabase } from "@/integrations/supabase/client";
+
+const { data: response } = await supabase
+  .from('identity_alignment_sessions')
+  .select('*');
+
 console.log('API Response:', response);
